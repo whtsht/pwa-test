f2875d22ff72e3773c8873c4c4e4d77a64139b94
Toma <whiteshirt0079@gmail.com>
2022-09-11 11:43:35 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("sw.js")
+        .then((registration) => {
+            console.log("SW Registered!");
+            console.log(registration);
+        })
+        .catch((error) => {
+            console.log("SW Registration Failed");
+            console.log(error);
+        });
+} else {
+    alert("serviceWorker does not support.");
+}
